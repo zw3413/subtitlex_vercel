f04ee392a523563aa25a891433508d9060ccd17c
Elvin Zhang <zhangweicalm@gmail.com>
2024-04-26 17:32:19 +0800

    add api checkSubscription

---
+++ app/api/checkSubscription/route.js
@@ -0,0 +1,31 @@
+import Stripe from "stripe";
+import { NextResponse } from "next/server";
+import { PrismaClient } from "@prisma/client";
+const prisma = new PrismaClient();
+
+export async function GET(request) {
+  const stripe = new Stripe(process.env.STRIPE_SECRET);
+
+  const email = request.nextUrl.searchParams.get("email");
+
+  const user = await prisma.user.findFirst({
+    where: {
+      email: email,
+    },
+  });
+
+  let userInfo 
+  if(user){
+    const subscriptions = await stripe.subscriptions.list({
+        customer :user?.stripe_customer_id
+    })
+    const hasSub = subscriptions?.data?.length > 0;
+    userInfo = {
+        email: user.email,
+        name: user.name,
+        hasSub: hasSub,
+        expireDate: hasSub ? subscriptions?.data[0].current_period_end : null,
+      };
+  }
+  return NextResponse.json(userInfo);
+}
