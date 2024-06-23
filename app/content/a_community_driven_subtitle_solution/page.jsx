1b54b75ca268e9140091261236f65e4aa6f7f99d
Elvin Zhang <zhangweicalm@gmail.com>
2024-06-24 02:53:52 +0800

    twitter image

--- app/content/a_community_driven_subtitle_solution/page.jsx
+++ app/content/a_community_driven_subtitle_solution/page.jsx
@@ -1,5 +1,14 @@
-"use client"
+
 import Content from './content.mdx'
+export async function generateMetadata(context) {
+return {
+  title: "Enhance Your Viewing Experience with Subtitlex : A Community-Driven Subtitle Solution",
+  description: `Chrome Extension provide the function of load subtitles from user local and SubtitleX library`,
+  twitter:{
+    image: "https://www.subtitlex.xyz/images/blog/img1.png"
+  }
+};
+}
 
 export default function Page() {
   return (
