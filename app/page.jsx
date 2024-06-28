9f5b1d23b0bdbdfa1ae37ddcd9e1b3c516bfa18e
zw3413 <zhangweicalm@gmail.com>
2024-06-28 23:36:33 +0800

    rewrite 2

--- app/page.jsx
+++ app/page.jsx
@@ -9,40 +9,49 @@ import { useEffect } from "react";
 import initLensBlur from "./(components)/lensblur/LensBlur"
 
 export default function Home() {
+
+  //detect the domain, if contains "jav", then redirect to /jav
+
+  const url = window.location;
+  if (url.hostname === 'jav.subtitlex.xyz') {
+    window.location.href = '/jav';
+  }
+
+
   const session = useSession();
   UpdateAndGetUser(session);
 
   useEffect(() => {
-   /// document.documentElement.className = "js";
-    if(! document.querySelector('canvas') ){
+    /// document.documentElement.className = "js";
+    if (!document.querySelector('canvas')) {
       initLensBlur(document.querySelector('#lensblur'));
-    }  
+    }
   });
   return (
     <>  <div id="lensblur" className="z-0 absolute"></div>
-    <div className="container min-w-[1025px] z-10 float-left">
-      <script
-        type="module"
-        src="/lensblur/assets/index-WHyTcPwp.js"
-       async/>
-      <link
-        rel="stylesheet"
-        href="/lensblur/assets/index-hamleSwx.css"
-      ></link>
-     
-   
-      <Image
-        alt="subtitlex"
-        className="mx-auto"
-        src="/images/subtitlex-512-white-transparent.png"
-        width={250}
-        height={250}
-      />
-      <SearchForm />
-      <div className="flex place-content-center mt-20">
-        {" "}
-        <InstallChromeExtensionButton></InstallChromeExtensionButton>
-      </div>
-    </div></>
+      <div className="container min-w-[1025px] z-10 float-left">
+        <script
+          type="module"
+          src="/lensblur/assets/index-WHyTcPwp.js"
+          async />
+        <link
+          rel="stylesheet"
+          href="/lensblur/assets/index-hamleSwx.css"
+        ></link>
+
+
+        <Image
+          alt="subtitlex"
+          className="mx-auto"
+          src="/images/subtitlex-512-white-transparent.png"
+          width={250}
+          height={250}
+        />
+        <SearchForm />
+        <div className="flex place-content-center mt-20">
+          {" "}
+          <InstallChromeExtensionButton></InstallChromeExtensionButton>
+        </div>
+      </div></>
   );
 }
