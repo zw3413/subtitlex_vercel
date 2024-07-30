6a6e77c8eadc76c643a9f9498bcdb3a0cc7fa40f
Elvin Zhang <zhangweicalm@gmail.com>
2024-07-30 17:47:33 +0800

    minor update the i18n

--- app/[lng]/(components)/resultDetailSubscribeInstruct.jsx
+++ app/[lng]/(components)/resultDetailSubscribeInstruct.jsx
@@ -23,6 +23,7 @@ export default async function ResultDetailSubscribeInstruct({ subText, lng }) {
             </Link>
             to unlock the limitation. 
           </Trans>
+         <bt/> 👋 {t('proxy_tip')}
         </div>
       ) : (
         <></>
