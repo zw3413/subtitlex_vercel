301baaedf0b1568171ce7d1c12681f0cb5803d1c
Elvin Zhang <zhangweicalm@gmail.com>
2024-08-16 13:39:51 +0800

    fix

--- app/[lng]/auth/signin/SignInForm.jsx
+++ app/[lng]/auth/signin/SignInForm.jsx
@@ -36,7 +36,7 @@ export default function SignInForm({ providers, lng }) {
           required
           className="text-black w-full text-base font-medium rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
         />
-        {providers.credentials && (
+        {/* {providers.credentials && (
           <input
             type="password"
             value={password}
@@ -45,7 +45,7 @@ export default function SignInForm({ providers, lng }) {
             required
             className="w-full rounded-md border border-gray-300 px-3 py-2 placeholder-gray-400 focus:border-indigo-500 focus:outline-none focus:ring-indigo-500"
           />
-        )}
+        )} */}
         <button
           type="submit"
           className="w-full text-base font-medium rounded-md bg-indigo-600 py-2 px-4 text-white hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-indigo-500 focus:ring-offset-2"
