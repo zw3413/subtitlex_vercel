c5197bb6a6628820fac2a5cfbcada10ced3915a1
Elvin Zhang <zhangweicalm@gmail.com>
2024-08-16 13:57:28 +0800

    fallback to default next-auth page

--- app/api/auth/[...nextauth]/options.js
+++ app/api/auth/[...nextauth]/options.js
@@ -129,13 +129,13 @@ export const options = {
     //   },
     // }),
   ],
-  pages: {
-    signIn: "/auth/signin",
-    signOut: '/auth/signout',
-    error: '/auth/error', // Error code passed in query string as ?error=
-    verifyRequest: '/auth/verify-request', // (used for check email message)
-    //newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
-  },
+  // pages: {
+  //   signIn: "/auth/signin",
+  //   signOut: '/auth/signout',
+  //   error: '/auth/error', // Error code passed in query string as ?error=
+  //   verifyRequest: '/auth/verify-request', // (used for check email message)
+  //   //newUser: '/auth/new-user' // New users will be directed here on first sign in (leave the property out if not of interest)
+  // },
   callbacks: {
     session: async ({ session, token, user }) => {
       //console.log("Session Callback:", {session, token, user});
